500a6843a16edc74d54e3652a27c8b3ed569203c
baris69 <bxrxsss5@gmail.com>
2024-10-24 19:41:21 +0200

    Delete main.lua

--- main.lua
+++
@@ -1,6 +0,0 @@
-local playerGui = game.Players.LocalPlayer:WaitForChild("PlayerGui")
-local screenGui = playerGui:FindFirstChild("ScreenGui")  -- Assuming the ScreenGui's name is "ScreenGui"
-
-if screenGui then
-    screenGui:Destroy()  -- Destroys the "Hi" GUI
-end
